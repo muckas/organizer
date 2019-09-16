cc95d48e85abfc8bd1b583bbd12ebf38b22ab5aa
muckas <motobike8@outlook.com>
2019-09-17 01:59:35 +0500

    removed print statement

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ def main():
   tasks = None
   with open('tasks.txt', 'r+') as f:
     tasks = f.readlines()
-  print(tasks)
   result = ''
   for line in tasks:
     result += (f'<pre class="tab">{line}</pre>')
