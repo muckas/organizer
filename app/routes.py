bbaeb5e6b5e6f7191be4bd7556e6523182c2ad8e
muckas <motobike8@outlook.com>
2019-09-18 00:31:35 +0500

    Make index function show tasks properly
    Added logic to index template to render potentialy unlimited number of
    tasks

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,4 @@ def index():
   tasks = None
   with open('tasks.txt', 'r+') as f:
     tasks = f.readlines()
-  tasklist = ''
-  for line in tasks:
-    tasklist += (f'<pre class="tab">{line}</pre>')
-  return render_template('index.html', title='Tasks', tasklist=tasklist)
+  return render_template('index.html', title='Tasks', tasks=tasks)
