4b4e390e4047e84cee52f20b0abdb631e5a09cb5
muckas <motobike8@outlook.com>
2019-09-19 00:27:38 +0500

    Make Sign In page do a fake login
    Sign In page now redirects to index showing the username selected and
    reports if any nessesary field is blank

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
 
 @app.route('/')
+@app.route('/index')
 @app.route('/tasks')
 def index():
   tasks = None
@@ -11,7 +12,11 @@ def index():
     tasks = f.readlines()
   return render_template('tasks.html', title='Tasks', tasks=tasks)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for user {}, remember_me={}'.format(
+      form.username.data, form.remember_me.data))
+    return redirect('/index')
   return render_template('login.html', title='Sign In', form=form)
