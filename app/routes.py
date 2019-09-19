6ac016901c1d4459f9adb36c4902d4f1491003c3
muckas <motobike8@outlook.com>
2019-09-20 04:56:24 +0500

    Remove unused code from develop
    Removed login code in preparation of v0.2. All of the removed code got
    branched out to 'login-page'

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, redirect, url_for
 from app import app
-from app.forms import LoginForm
 
 
 @app.route('/')
@@ -19,12 +18,3 @@ def tasks():
     else:
       timeframe += task
   return render_template('tasks.html', title='Tasks', timeframes=timeframes)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-    flash('Login requested for user {}, remember_me={}'.format(
-      form.username.data, form.remember_me.data))
-    return redirect(url_for('tasks'))
-  return render_template('login.html', title='Sign In', form=form)
