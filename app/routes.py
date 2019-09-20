d408f2795b31d8be66b6b8e90b47b1ba9944526d
muckas <motobike8@outlook.com>
2019-09-20 19:46:39 +0500

    Add fully working editing page
    Now there is an edit page in whitch you can edit tasks and save them

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from app import app
+from app.forms import EditForm
 
 
 @app.route('/')
@@ -19,3 +20,17 @@ def tasks():
       timeframe += task
   timeframes.append(timeframe)
   return render_template('tasks.html', title='Tasks', timeframes=timeframes)
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit_tasks():
+  form = EditForm()
+  if form.validate_on_submit():
+    data = form.content.data
+    with open('tasks.txt', 'w') as f:
+      f.write(data)
+    return redirect(url_for('tasks'))
+  elif request.method == 'GET':
+    with open('tasks.txt', 'r') as f:
+      data = f.read()
+    form.content.data = data
+  return render_template('edit.html', form = form)
