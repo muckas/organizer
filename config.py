46b236e47284c936638f7773faf4066129b5d027
muckas <motobike8@outlook.com>
2019-09-26 00:45:42 +0500

    Add basic authentication and config

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import os
+import configparser
+
+configParser = configparser.RawConfigParser()
+configFilePath = r'config.cfg'
+configParser.read(configFilePath)
 
 class Config(object):
-  SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
+  SECRET_KEY = configParser.get('Settings', 'SECRET_KEY')
+  BASIC_AUTH_USERNAME = configParser.get('Settings', 'BASIC_AUTH_USERNAME')
+  BASIC_AUTH_PASSWORD = configParser.get('Settings', 'BASIC_AUTH_PASSWORD')
+  BASIC_AUTH_FORCE = configParser.get('Settings', 'BASIC_AUTH_FORCE')
