7ae3a19af6587523e9b5729412d18f391334038b
muckas <motobike8@outlook.com>
2019-09-26 17:19:52 +0500

    Add dropdown with a list of files in tasks/
    Moved edit button from navigation to the main page and added dropdown
    menu that lets you change URLs for different files in tasks folder, but
    dows not yet navigate to this tasks

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template, redirect, url_for, request
 from app import app
 from app.forms import EditForm
@@ -6,21 +7,25 @@ from config import Config
 
 @app.route('/')
 @app.route('/index')
-@app.route('/tasks')
-def tasks():
+@app.route('/tasks/')
+@app.route('/tasks/<task>')
+def tasks(task=None):
+  files = os.listdir(Config.TASKS_PATH)
   tasks = None
-  with open('tasks.txt', 'r') as f:
-    tasks = f.readlines()
-  timeframes = []
-  timeframe = tasks[0]
-  for task in tasks[1:]:
-    if task == '\n':
-      timeframes.append(timeframe)
-      timeframe = ''
-    else:
-      timeframe += task
-  timeframes.append(timeframe)
-  return render_template('tasks.html', title='Tasks', timeframes=timeframes)
+  if task:
+    with open('tasks.txt', 'r') as f:
+      tasks = f.readlines()
+    timeframes = []
+    timeframe = tasks[0]
+    for task in tasks[1:]:
+      if task == '\n':
+        timeframes.append(timeframe)
+        timeframe = ''
+      else:
+        timeframe += task
+    timeframes.append(timeframe)
+    return render_template('tasks.html', title='Tasks', timeframes=timeframes, files=files)
+  return render_template('tasks.html', title='Tasks', files=files)
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit_tasks():
