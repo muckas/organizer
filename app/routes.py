85bff2202bcef4e4994d2ca90794db635b9bf08b
muckas <motobike8@outlook.com>
2019-10-03 21:02:31 +0500

    Restructure Config and automate base template
    In config.ini there is a variable for content path and a list with all
    the folders it needs now
    
    Base template now gets all the links from config

--- app/routes.py
+++ app/routes.py
@@ -10,13 +10,11 @@ from config import Config
 @app.route('/<folder>/')
 @app.route('/<folder>/<name>')
 def show(folder, name=None):
-  if folder == 'tasks':
-    path = Config.TASKS_PATH
-  elif folder == 'notes':
-    path = Config.NOTES_PATH
-  files = os.listdir(path)
+  path = Config.CONTENT_PATH
+  folders = Config.CONTENT_FOLDERS
+  files = os.listdir(os.path.join(path, folder))
   if name:
-    with open(os.path.join(path, name), 'r') as f:
+    with open(os.path.join(path, folder, name), 'r') as f:
       lines = f.readlines()
 
     if folder == 'tasks':
@@ -32,61 +30,54 @@ def show(folder, name=None):
       content.append(timeframe)
 
     elif folder == 'notes':
-      content = ''
+      content = str()
       for line in lines:
         content += line
 
     return render_template('show.html',
-        title=folder.title, content=content, files=files, folder=folder, name=name)
-
-  return render_template('show.html', title=folder.upper(), folder=folder, files=files)
+        title=folder.title, content=content, files=files, folders=folders, folder=folder, name=name)
+  return render_template('show.html', title=folder.upper(), folders=folders, folder=folder, files=files)
 
 @app.route('/edit/<folder>/<name>', methods=['GET', 'POST'])
 def edit(folder, name):
-  if folder == 'tasks':
-    path = Config.TASKS_PATH
-  elif folder == 'notes':
-    path = Config.NOTES_PATH
+  path = Config.CONTENT_PATH
+  folders = Config.CONTENT_FOLDERS
   form = EditForm()
   if form.validate_on_submit():
     data = form.content.data
-    with open(os.path.join(path, name), 'w') as f:
+    with open(os.path.join(path, folder, name), 'w') as f:
       f.write(data)
     return redirect(url_for('show', folder=folder, name=name))
   elif request.method == 'GET':
-    with open(os.path.join(path, name), 'r') as f:
+    with open(os.path.join(path, folder, name), 'r') as f:
       data = f.read()
     form.content.data = data
-  return render_template('form.html', title=f'Edit {name}', form=form)
+  return render_template('form.html', title=f'Edit {name}', folders=folders, form=form)
 
 @app.route('/add/<folder>', methods=['GET', 'POST'])
 def add(folder):
-  if folder == 'tasks':
-    path = Config.TASKS_PATH
-  elif folder == 'notes':
-    path = Config.NOTES_PATH
+  path = Config.CONTENT_PATH
+  folders = Config.CONTENT_FOLDERS
   form = AddForm()
   if form.validate_on_submit():
     name = form.name.data
     content = form.content.data
-    with open(os.path.join(path, name), 'x') as f:
+    with open(os.path.join(path, folder, name), 'x') as f:
       f.write(content)
     return redirect(url_for('show', folder=folder, name=name))
   elif request.method == 'GET':
-    return render_template('form.html', title='Add list', form=form)
+    return render_template('form.html', title='Add list', folders=folders, form=form)
 
 @app.route('/remove/<folder>/<name>', methods=['GET', 'POST'])
 def remove(folder, name):
-  if folder == 'tasks':
-    path = Config.TASKS_PATH
-  elif folder == 'notes':
-    path = Config.NOTES_PATH
+  path = Config.CONTENT_PATH
+  folders = Config.CONTENT_FOLDERS
   form = DelForm()
   if form.validate_on_submit():
     result = form.result.data
     if result == 'yes':
-      os.remove(os.path.join(path, name))
+      os.remove(os.path.join(path, folder, name))
       return redirect(url_for('show', folder=folder))
     return redirect(url_for('show', folder=folder, name=name))
   elif request.method == 'GET':
-    return render_template('form.html', title=f'Delete {name}?', form=form)
+    return render_template('form.html', title=f'Delete {name}?', folders=folders,  form=form)
--- config.py
+++ config.py
@@ -17,11 +17,12 @@ class Config(object):
   BASIC_AUTH_PASSWORD = configParser.get('Security', 'BASIC_AUTH_PASSWORD')
   BASIC_AUTH_FORCE = configParser.get('Security', 'BASIC_AUTH_FORCE')
 
-  TASKS_PATH = configParser.get('Content', 'TASKS_PATH')
-  NOTES_PATH = configParser.get('Content', 'NOTES_PATH')
+  CONTENT_PATH = configParser.get('Content', 'CONTENT_PATH')
+  CONTENT_FOLDERS = eval(configParser.get('Content', 'CONTENT_FOLDERS'))
 
   def make_path():
-    content = configParser['Content']
-    for folder in content:
-      if not os.path.isdir(content[folder]):
-        os.makedirs(content[folder])
+    path = configParser.get('Content', 'CONTENT_PATH')
+    folders = eval(configParser.get('Content', 'CONTENT_FOLDERS'))
+    for folder in folders:
+      if not os.path.isdir(os.path.join(path,folder)):
+        os.makedirs(os.path.join(path,folder))
