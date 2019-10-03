3c8cc522d6681890e620a4fc24e4f1132343e73f
muckas <motobike8@outlook.com>
2019-10-03 18:58:31 +0500

    Implement all buttons for notes

--- app/routes.py
+++ app/routes.py
@@ -8,13 +8,13 @@ from config import Config
 @app.route('/')
 @app.route('/index')
 @app.route('/tasks/')
-@app.route('/tasks/<task>')
-def tasks(task=None):
+@app.route('/tasks/<name>')
+def tasks(name=None):
   path = Config.TASKS_PATH
   files = os.listdir(path)
   tasks = None
-  if task:
-    with open(os.path.join(path, task), 'r') as f:
+  if name:
+    with open(os.path.join(path, name), 'r') as f:
       tasks = f.readlines()
     timeframes = []
     if len(tasks) > 0:
@@ -26,62 +26,71 @@ def tasks(task=None):
         else:
           timeframe += line
       timeframes.append(timeframe)
-    return render_template('tasks.html', title='Tasks', timeframes=timeframes, files=files, task=task)
+    return render_template('tasks.html', title='Tasks', timeframes=timeframes, files=files, name=name)
   return render_template('tasks.html', title='Tasks', files=files)
 
-@app.route('/edit/<task>', methods=['GET', 'POST'])
-def edit_tasks(task):
-  path = Config.TASKS_PATH
+@app.route('/edit/<folder>/<name>', methods=['GET', 'POST'])
+def edit(folder, name):
+  if folder == 'tasks':
+    path = Config.TASKS_PATH
+  elif folder == 'notes':
+    path = Config.NOTES_PATH
   form = EditForm()
   if form.validate_on_submit():
     data = form.content.data
-    with open(os.path.join(path, task), 'w') as f:
+    with open(os.path.join(path, name), 'w') as f:
       f.write(data)
-    return redirect(url_for('tasks', task=task))
+    return redirect(url_for(folder, name=name))
   elif request.method == 'GET':
-    with open(os.path.join(path, task), 'r') as f:
+    with open(os.path.join(path, name), 'r') as f:
       data = f.read()
     form.content.data = data
-  return render_template('form.html', title=f'Edit {task}', form=form)
+  return render_template('form.html', title=f'Edit {name}', form=form)
 
-@app.route('/add_tasks', methods=['GET', 'POST'])
-def add_tasks():
-  path = Config.TASKS_PATH
+@app.route('/add/<folder>', methods=['GET', 'POST'])
+def add(folder):
+  if folder == 'tasks':
+    path = Config.TASKS_PATH
+  elif folder == 'notes':
+    path = Config.NOTES_PATH
   form = AddForm()
   if form.validate_on_submit():
     name = form.name.data
     content = form.content.data
     with open(os.path.join(path, name), 'x') as f:
       f.write(content)
-    return redirect(url_for('tasks', task=name))
+    return redirect(url_for(folder, name=name))
   elif request.method == 'GET':
     return render_template('form.html', title='Add list', form=form)
 
-@app.route('/del/<task>', methods=['GET', 'POST'])
-def del_tasks(task):
+@app.route('/remove/<folder>/<name>', methods=['GET', 'POST'])
+def remove(folder, name):
+  if folder == 'tasks':
+    path = Config.TASKS_PATH
+  elif folder == 'notes':
+    path = Config.NOTES_PATH
   form = DelForm()
   if form.validate_on_submit():
     result = form.result.data
-    path = Config.TASKS_PATH
     if result == 'yes':
-      os.remove(os.path.join(path, task))
-      return redirect(url_for('tasks'))
-    return redirect(url_for('tasks', task=task))
+      os.remove(os.path.join(path, name))
+      return redirect(url_for(folder))
+    return redirect(url_for(folder, name=name))
   elif request.method == 'GET':
-    return render_template('form.html', title=f'Delete {task}?', form=form)
+    return render_template('form.html', title=f'Delete {name}?', form=form)
 
 @app.route('/notes/')
-@app.route('/notes/<note>')
-def notes(note=None):
+@app.route('/notes/<name>')
+def notes(name=None):
   path = Config.NOTES_PATH
   files = os.listdir(path)
-  if note:
-    with open(os.path.join(path, note), 'r') as f:
+  if name:
+    with open(os.path.join(path, name), 'r') as f:
       lines = f.readlines()
     content = ''
     for line in lines:
       content += line
-    return render_template('notes.html', title='Notes', content=content, files=files, note=note)
+    return render_template('notes.html', title='Notes', content=content, files=files, name=name)
   return render_template('notes.html', title='Notes', files=files)
 
 
